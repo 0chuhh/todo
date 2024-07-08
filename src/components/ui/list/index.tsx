3d697e62be5b6aca5613a6babaa508b06224a07c
Viktor <vvlasovdo@mail.ru>
2024-07-08 14:23:05 +0900

    feat: add list component

---
+++ src/components/ui/list/index.tsx
@@ -0,0 +1,41 @@
+import { Fragment } from 'react/jsx-runtime';
+import classes from './style.module.scss';
+import { defaultGetItemLabel, defaultRenderItem } from './utils';
+import { generateKey } from 'utils/generate-key';
+
+export interface IListProps<Value> {
+    items: Value[];
+    getItemLabel?: typeof defaultGetItemLabel<Value>;
+    renderItem?: typeof defaultRenderItem<Value>;
+
+}
+
+export function List<Value>({ 
+    items, 
+    getItemLabel = defaultGetItemLabel,
+    renderItem = defaultRenderItem
+}: IListProps<Value>) {
+
+    const handleItemClick = (item:Value) => {
+        console.log(item);
+    }
+
+    return (
+        <div className={classes.list}>
+            {
+                items.map((item, index)=>(
+                    <Fragment key={generateKey(index)}>
+                        {
+                            renderItem(item, {
+                                className:classes.item,
+                                value:item,
+                                label:`${getItemLabel(item)}`,
+                                onClick:handleItemClick
+                            })
+                        }
+                    </Fragment>
+                ))
+            }
+        </div>
+    );
+}
---
+++ src/components/ui/list/item/index.tsx
@@ -0,0 +1,15 @@
+import { useCallback } from 'react';
+
+export interface IListItemProps<Value> {
+    label: string;
+    value:Value;
+    className:string;
+    onClick:(value:Value)=>void;
+}
+
+export function ListItem<Value>({ label, value, className, onClick }: IListItemProps<Value>) {
+    const handleClick = useCallback(()=> onClick(value),[value])
+    return (
+        <div onClick={handleClick} className={className}>{label}</div>
+    );
+}
