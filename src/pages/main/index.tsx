05b6ab4aec9db07af2393caa0966677524961bf7
Viktor <vvlasovdo@mail.ru>
2024-07-08 10:31:23 +0900

    feat: add main page

---
+++ src/pages/main/index.tsx
@@ -0,0 +1,9 @@
+import { PageContainer } from "components/ui/page-container";
+
+export const MainPage = () => {
+    return (
+        <PageContainer title="todos">
+            hello
+        </PageContainer>
+    )
+}
