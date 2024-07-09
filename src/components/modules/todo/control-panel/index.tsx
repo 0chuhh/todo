6731ebc9d557990d426d10ee8d247520a4f2c9d6
Viktor <vvlasovdo@mail.ru>
2024-07-09 10:36:00 +0900

    feat: add filter events

--- src/components/modules/todo/control-panel/index.tsx
+++ src/components/modules/todo/control-panel/index.tsx
@@ -1,27 +1,28 @@
-import { Button, ButtonGroup, ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { Button, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import { TodoFilter, TodoStatus } from 'models/todo';
 import React, { FC } from 'react';
-import classes from './style.module.scss'
+import classes from './style.module.scss';
 
 export interface ITodoControlPanelProps {
     leftTodos: number;
     status: TodoFilter;
-    onChange:(
+    onFilterChange: (
         event: React.MouseEvent<HTMLElement>,
-        newStatus: string | null,
-      )=>void
+        newStatus: TodoFilter,
+    ) => void;
+    onClearCompleted: () => void
 }
 
-export const TodoControlPanel: FC<ITodoControlPanelProps> = ({ leftTodos, status }) => {
+export const TodoControlPanel: FC<ITodoControlPanelProps> = React.memo(({ leftTodos, status, onFilterChange, onClearCompleted }) => {
     return (
         <div className={classes.control}>
             {leftTodos} items left
-            <ToggleButtonGroup onChange={} value={status}>
+            <ToggleButtonGroup exclusive onChange={onFilterChange} value={status}>
                 <ToggleButton value={'ALL'}>All</ToggleButton>
                 <ToggleButton value={TodoStatus.ACTIVE}>Active</ToggleButton>
                 <ToggleButton value={TodoStatus.COMPLETED}>Completed</ToggleButton>
             </ToggleButtonGroup>
-            <Button>Clear completed</Button>
+            <Button onClick={onClearCompleted}>Clear completed</Button>
         </div>
     );
-};
+});
--- src/components/modules/todo/index.tsx
+++ src/components/modules/todo/index.tsx
@@ -1,10 +1,8 @@
 import { List } from 'components/ui/list';
 import { ITodo, TodoFilter, TodoStatus } from 'models/todo';
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { TodoInput } from './input';
 import { useLocalStorage } from 'hooks/use-local-storage';
-import { ListItem } from 'components/ui/list/item';
-import { Checkbox } from '@mui/material';
 import { TodoItem } from './item';
 import classes from './style.module.scss';
 import { TodoControlPanel } from './control-panel';
@@ -12,12 +10,12 @@ import { TodoControlPanel } from './control-panel';
 const items: ITodo[] = [
     {
         id: 0,
-        text: 'some text',
+        text: 'test todo',
         status: TodoStatus.ACTIVE,
     },
     {
         id: 1,
-        text: 'some text',
+        text: 'test todo 2',
         status: TodoStatus.ACTIVE,
     }
 ];
@@ -25,45 +23,59 @@ const items: ITodo[] = [
 export const Todo = () => {
     const [todoList, setTodoList] = useLocalStorage<ITodo[]>('todos', items);
 
-    const [ filterStatus, setFilterStatus ] = useState<TodoFilter>('ALL')
+    const [filterStatus, setFilterStatus] = useState<TodoFilter>('ALL');
 
-    const addTodo = (todo: ITodo) => setTodoList(prev => [...prev, todo]);
+    const activeTodosCount = useMemo(()=>todoList.filter(todo=>todo.status===TodoStatus.ACTIVE).length,[todoList])
 
-    const handleComplete = (item: ITodo) => () => {
+    const addTodo = useCallback((todo: ITodo) => setTodoList(prev => [...prev, todo]),[todoList]);
+
+    const handleComplete = useCallback((item: ITodo) => {
         const status = item.status === TodoStatus.ACTIVE ? TodoStatus.COMPLETED : TodoStatus.ACTIVE;
         setTodoList((prev) => prev.map(i => i.id === item.id ? { ...i, status } : i));
-    };
+    },[todoList]);
 
-    const handleFilter = (status: 'ALL' | keyof typeof TodoStatus)=>(items: ITodo[]): ITodo[] => {
-        switch (status) {
+    const handleChangeFilterStatus = useCallback((_event: React.MouseEvent<HTMLElement>, newStatus: TodoFilter) => {
+        setFilterStatus(newStatus);
+    },[]);
+
+    const filterTodos = useCallback((items: ITodo[]): ITodo[] => {
+        switch (filterStatus) {
             case 'ACTIVE':
                 return items.filter(i => i.status === TodoStatus.ACTIVE);
 
             case 'COMPLETED':
                 return items.filter(i => i.status === TodoStatus.COMPLETED);
-                
+
             default:
                 return items;
         }
-    };
+    }, [filterStatus]);
+
+    const handleClearCompleted = useCallback(() => {
+        setTodoList(prev=>prev.filter(todo=>todo.status !== TodoStatus.COMPLETED));
+    },[todoList]);
 
     return (
         <div className={classes.wrapper}>
             <TodoInput addTodo={addTodo} />
             <List
+                className={classes.list}
                 getItemLabel={(item) => item.text}
                 items={todoList}
-                filter={handleFilter(filterStatus)}
-                renderItem={(item, { className, ...props }) => (
+                filter={filterTodos}
+                onItemClick={handleComplete}
+                renderItem={(_item, props) => (
                     <TodoItem
                         {...props}
-                        todoItem={item}
-                        className={className}
-                        onComplete={handleComplete}
                     />
                 )}
             />
-            <TodoControlPanel leftTodos={todoList.length} status={filterStatus} />
+            <TodoControlPanel
+                onFilterChange={handleChangeFilterStatus}
+                leftTodos={activeTodosCount}
+                status={filterStatus}
+                onClearCompleted={handleClearCompleted}
+            />
         </div>
     );
 };
